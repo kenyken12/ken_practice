f6f44be751836e0dd400088d69703cd3802cff41
keny <kenywasyourpapi@gmail.com>
2026-09-16 13:08:25 -0400

    first day of coding, reciept

--- Untitled-2.py
+++ Untitled-2.py
@@ -1,1 +1,23 @@
-print("helloworld")
+print("**********BREW&BYTE*************\n")
+
+#input from customer
+name = input("hello!, can i get your name?:  ").title().strip()
+order = input("what can I get for you?:  ").strip()
+
+#taxes, tips allat
+total = 5
+tax = total * 0.0625
+tip = total * 0.18
+newTotal = total + tax + tip 
+
+#item code 
+itemCode = f"{order[:3]}-{len(order)}"
+
+print(f"CUSTOMER: {name}\nORDER: {order}\nITEM CODE: {itemCode}\nTOTAL - {round(newTotal, 2)}\n")
+
+print("**********GOODBYE*************")
+
+
+
+
+
