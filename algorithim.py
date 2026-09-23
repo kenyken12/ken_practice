7069ecc14f7990108d628a330dd3e1591679e079
keny <kenywasyourpapi@gmail.com>
2026-09-23 13:12:43 -0400

    notes 9/23

--- algorithim.py
+++ algorithim.py
@@ -1,1 +1,21 @@
-print("hey, its me, its verity")
+"""
+n = represents input size 
+when n increases, the amount of work can increase not linearly depending on the algorithm 
+
+
+to analyze a algorithm, we count the amount of basic operations the code does 
+
+basic operations include...
+- arithmetic operations
+- comparison
+- reading or writing values 
+
+we're interested in how the number of operations changes as the input gets bigger
+
+constant time O(1) = no matter how much input is added, the amount of work is the same
+linear time O(n) = the amount of work porpotionally rises with the input size
+
+
+
+
+"""
